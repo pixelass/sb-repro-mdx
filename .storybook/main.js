041388f5f2af285f897e2b7730ca887ebf78c384
Gregor Adams <greg@pixelass.com>
2022-06-20 21:10:16 +0200

    Reproduce MDX issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,26 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+        sourceLoaderOptions: null,
+        transcludeMarkdown: true,
+      },
+    },
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  features: {
+    previewMdx2: true,
+  },
+  core: {
+    builder: "webpack5",
+  },
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# This file will not work
+
+<!-- Because of this comment -->
+
+The following error occurs
+
+```shell
+
+Unexpected character `!` (U+0021) before name, expected a character that can start a name, such as a letter, `$`, or `_` (note: to create a comment in MDX, use `{/* text */}`)
+
+```
